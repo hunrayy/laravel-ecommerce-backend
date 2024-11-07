6215eac76b259e871443c906a82e66af4b27b1b8
hunrayy <henryokiyi8@gmail.com>
2024-11-07 16:34:42 +0100

    updated code

--- app/Http/Middleware/VerifyJWTToken.php
+++ app/Http/Middleware/VerifyJWTToken.php
@@ -30,6 +30,8 @@ class VerifyJWTToken
         try {
             // Split the bearer token
             $bearerToken = explode(' ', $authorization)[1];
+            // test code
+            // test code 
 
             // Verify the token
             $decodedToken = JWT::decode($bearerToken, new Key(env("JWT_SECRET"), 'HS256'));
